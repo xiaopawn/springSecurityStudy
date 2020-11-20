8c81817b9304bc10f262aaf6ad8d759d829f8362
pawn <842384656@qq.com>
2020-11-20 21:03:21 +0800

    '验证码继承功能'

--- basic-server/src/main/java/com/pawn/courses/config/auth/imagecode/CaptchaCode.java
+++ basic-server/src/main/java/com/pawn/courses/config/auth/imagecode/CaptchaCode.java
@@ -1,22 +1,11 @@
 package com.pawn.courses.config.auth.imagecode;
 
-import lombok.Data;
+import com.pawn.courses.config.auth.common.Code;
 
-import java.time.LocalDateTime;
-
-@Data
-public class CaptchaCode {
-
-    private String code;
-
-    private LocalDateTime expireTime;
+public class CaptchaCode extends Code {
 
     public CaptchaCode(String code,int expiredAfterSeconds){
-        this.code = code;
-        this.expireTime = LocalDateTime.now().plusSeconds(expiredAfterSeconds);
+      super(code,expiredAfterSeconds);
     }
 
-    public boolean isExpired(){
-        return LocalDateTime.now().isAfter(expireTime);
-    }
 }
